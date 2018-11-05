b16de547f810dbc0fc05a6889d0ba42c05eb02de
Margeir <margeirf@gmail.com>
2018-11-05 13:35:32 +0000

    Add files via upload
    Kláraði að commenta

--- Verk5/DrumKit/DOM/main.js
+++ Verk5/DrumKit/DOM/main.js
@@ -1,12 +1,19 @@
-<div id="elem">
-  <div id="elem-content">Element</div>
-</div>
+// #1. id="age-table"
+let table = document.getElementById('age-table') // 
 
-<script>
-  alert(elem); // DOM-element with id="elem"
-  alert(window.elem); // accessing global variable like this also works
+// #2. All label elements inside that table
+table.getElementsByTagName('label') //Leitar í table og finnur alla með sama label
 
-  // for elem-content things are a bit more complex
-  // that has a dash inside, so it can't be a variable name
-  alert(window['elem-content']); // ...but accessible using square brackets [...]
-</script>
+// #3. The first td in that table (with the word "Age").
+table.getElementsByTagName('td')[0] //Leitar í table og finnur það fyrsta (0) mep tagName tb
+
+// #4. The form with the name "search".
+// assuming there's only one element with name="search"
+let form = document.getElementsByName('search')[0] //Skoðar öll nöfn og finnur fyrsta (0) með nafnið "search"
+
+// #5. The first input in that form.
+form.getElementsByTagName('input')[0] //Leitar í form og finnur fyrsta input (0)
+
+// #6. The last input in that form.
+let inputs = form.querySelectorAll('input') // leitar í öllum input
+inputs[inputs.length-1] // tekur seinasta input
